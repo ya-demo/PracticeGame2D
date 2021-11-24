b900185af456f7a62d5dce186e682e13ee2ed604
Bruce Yuan <bruce40713@gmail.com>
2021-11-24 12:25:57 +0800

    新增 選擇關卡畫面 及 切換Scene

--- Assets/Script/ButtonScript.cs
+++ Assets/Script/ButtonScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonScript : MonoBehaviour
 {
@@ -32,5 +33,6 @@ public class ButtonScript : MonoBehaviour
         myAnim.SetBool("Run", true);
         GameObject playButton = GameObject.Find("Canvas/SafeAreaPanel/PlayButton");
         playButton.SetActive(false);
+        SceneManager.LoadScene("SelectLevel");
     }
 }
